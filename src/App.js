8e08d7be530862d779e990154b3cc7f7e966a890
tercere <tercere@labcorp.com>
2022-02-16 13:56:08 -0400

    Uso de props y Card

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import {Navbar} from "react-bootstrap";
 import CatalogComponent from "./components/CatalogComponent";
+import {Component} from "react";
+import {ITEMS} from "./shared/items";
 
-function App() {
-  return (
-    <div>
-      <Navbar bg="dark" variant="dark">
-        <div className="container">
-          <Navbar.Brand href="/">
-            Sistema de Integrado Informacion Universitario
-          </Navbar.Brand>
-        </div>
-      </Navbar>
-      <CatalogComponent/>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      items: ITEMS
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <div className="container">
+            <Navbar.Brand href="/">
+              Sistema de Integrado Informacion Universitario
+            </Navbar.Brand>
+          </div>
+        </Navbar>
+        <CatalogComponent items={this.state.items}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/CatalogComponent.js
+++ src/components/CatalogComponent.js
@@ -6,48 +6,20 @@ class CatalogComponent extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      items: [
-        {
-          name: 'Certificado de Diplomado',
-          image: '/assets/images/certificadoDiplomado.png',
-          category: 'certificado',
-          label: 'diplomado',
-          price: '200',
-          description: 'El certificado de Diplomado es un documento que se otorga a quienes han logrado su titulacion en grado de diplomado'
-        },
-        {
-          name: 'Certificado de Maestria',
-          image: '/assets/images/certificadoMaestria.png',
-          category: 'certificado',
-          label: 'maestria',
-          price: '300',
-          description: 'El certificado de Maestria es un documento que se otorga a quienes han logrado su titulacion en grado de magister'
-        },
-        {
-          name: 'Certificado Supletorio',
-          image: '/assets/images/certificadoSupletorio.png',
-          category: 'certificado',
-          label: 'supletorio',
-          price: '400',
-          description: 'El certificado Supletorio es un documento que se otorga a quienes han justificado de forma pertinente la necesidad del supletorio'
-        }
-
-      ]
     }
   }
 
   render() {
-    let catalog = this.state.items.map(item => {
+    let catalog = this.props.items.map(item => {
       return (
-        <div key={item.id} className="col-6 mt-5">
-          <Card style={{width: '18rem'}}>
+        <div key={item.id} className="col-12 col-md-5 m-1">
+          <Card text='primary' style={{ width: '90%' }} className='bg-dark bg-opacity-25'>
             <Card.Header>
-              <Card.Img src={item.image} alt={item.name}/>
+              <Card.Img style={{width:'80%'}} className='m-4' src={item.image} alt={item.name}/>
+              <Card.ImgOverlay>
+                <Card.Title>{item.name}</Card.Title>
+              </Card.ImgOverlay>
             </Card.Header>
-            <Card.Body className="ml-5">
-              <Card.Subtitle>{item.name}</Card.Subtitle>
-              <p>{item.description}</p>
-            </Card.Body>
           </Card>
         </div>
       );

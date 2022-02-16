102d8807bde161e9f65ec898a157e3bdd2035bfc
tercere <tercere@labcorp.com>
2022-02-16 19:46:10 -0400

    paso de parametros aka (also known as -> datos) del componente padre al hijo

--- src/components/CatalogComponent.js
+++ src/components/CatalogComponent.js
@@ -6,38 +6,11 @@ class CatalogComponent extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      items: [
-        {
-          name: 'Certificado de Diplomado',
-          image: '/assets/images/certificadoDiplomado.png',
-          category: 'certificado',
-          label: 'diplomado',
-          price: '200',
-          description: 'El certificado de Diplomado es un documento que se otorga a quienes han logrado su titulacion en grado de diplomado'
-        },
-        {
-          name: 'Certificado de Maestria',
-          image: '/assets/images/certificadoMaestria.png',
-          category: 'certificado',
-          label: 'maestria',
-          price: '300',
-          description: 'El certificado de Maestria es un documento que se otorga a quienes han logrado su titulacion en grado de magister'
-        },
-        {
-          name: 'Certificado Supletorio',
-          image: '/assets/images/certificadoSupletorio.png',
-          category: 'certificado',
-          label: 'supletorio',
-          price: '400',
-          description: 'El certificado Supletorio es un documento que se otorga a quienes han justificado de forma pertinente la necesidad del supletorio'
-        }
-
-      ]
     }
   }
 
   render() {
-    let catalog = this.state.items.map(item => {
+    let catalog = this.props.items.map(item => {
       return (
         <div key={item.id} className="col-6 mt-5">
           <Card style={{width: '18rem'}}>

a0e228dfa5fe509670fb01326c47c60337c5b504
tercere <tercere@labcorp.com>
2022-02-16 20:35:01 -0400

    Uso de eventos

--- src/components/CatalogComponent.js
+++ src/components/CatalogComponent.js
@@ -6,6 +6,7 @@ class CatalogComponent extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
+      selectedItem: null
     }
   }
 
@@ -14,7 +15,8 @@ class CatalogComponent extends Component {
       return (
         <div key={item.id} className="col-12 col-md-5 m-1">
           <Card text='primary' style={{width: '90%'}}
-          className='bg-dark bg-opacity-25'>
+                className='bg-dark bg-opacity-25'
+          onClick={()=> {this.onItemSelect(item)}}>
             <Card.Header>
               <Card.Img src={item.image} alt={item.name}/>
               <Card.ImgOverlay>
@@ -30,9 +32,44 @@ class CatalogComponent extends Component {
         <div className="row">
           {catalog}
         </div>
+        <div className="row">
+          {this.renderItem(this.state.selectedItem)}
+        </div>
       </div>
     );
   }
+
+  onItemSelect(item) {
+    this.setState({
+      selectedItem: item
+    });
+  }
+
+  renderItem(item) {
+    if (item != null) {
+      return (
+        <div key={item.id} className="col-8 m-auto">
+          <Card>
+            <Card.Header>
+              <Card.Title>{item.name}</Card.Title>
+            </Card.Header>
+            <Card.Img style={{width: '80%'}} className='m-4' src={item.image} alt={item.name}/>
+            <Card.Body>
+              <Card.Text>
+                {item.requisites.map(requisite => (<div>{requisite}</div>))}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+        </div>
+      );
+    } else {
+      return (
+        <div/>
+      );
+    }
+  }
+
 }
 
 export default CatalogComponent;

bb147f45a3a84bcd14f65f894f8fdd1d0f4470a1
tercere <tercere@labcorp.com>
2022-02-21 19:01:10 -0400

    componentes funcionales

--- src/components/CatalogComponent.js
+++ src/components/CatalogComponent.js
@@ -1,35 +1,36 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Card} from "react-bootstrap";
-import ItemDetail from "./ItemdetailComponent";
 
-class CatalogComponent extends Component {
+function RenderMenuItem({item, onClick}) {
+  return (
+    <Card text="primary" style={{width: "90%"}} className="bg-dark bg-opacity-25"
+                onClick={()=>onClick(item.id)}>
+    <Card.Header>
+      <Card.Img style={{width: "80%"}} className="m-4" src={item.image} alt={item.name}/>
+      <Card.ImgOverlay>
+        <Card.Title>{item.name}</Card.Title>
+      </Card.ImgOverlay>
+    </Card.Header>
+  </Card>
+  )
+}
 
-  render() {
-    let catalog = this.props.items.map(item => {
-      return (
-        <div key={item.id} className="col-12 col-md-5 m-1">
-          <Card text='primary' style={{width: '90%'}} className='bg-dark bg-opacity-25'
-                onClick={() => {
-                  this.props.onClick(item.id)
-                }}>
-            <Card.Header>
-              <Card.Img style={{width: '80%'}} className='m-4' src={item.image} alt={item.name}/>
-              <Card.ImgOverlay>
-                <Card.Title>{item.name}</Card.Title>
-              </Card.ImgOverlay>
-            </Card.Header>
-          </Card>
-        </div>
-      );
-    });
+const CatalogComponent = (props) => {
+  let catalog = props.items.map(item => {
     return (
-      <div className="container">
-        <div className="row">
-          {catalog}
-        </div>
+      <div key={item.id} className="col-12 col-md-5 m-1">
+        <RenderMenuItem onClick={props.onClick} item={item}/>
       </div>
     );
-  }
+  });
+  return (
+    <div className="container">
+      <div className="row">
+        {catalog}
+      </div>
+    </div>
+  );
 }
+
 
 export default CatalogComponent;
--- src/components/ItemdetailComponent.js
+++ src/components/ItemdetailComponent.js
@@ -1,87 +1,78 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Card} from "react-bootstrap";
 
-class ItemDetail extends Component {
+const ItemDetail = (props) => {
+  return (
+    <div className="row">
+      <div className="col-12 col-md-5 m-1">
+        {/*{this.renderItem(this.props.item)}*/}
+        <RenderItem item={props.item}/>
+      </div>
+      <div className="col-12 col-md-5 m-1">
+        {/*{this.renderComments(this.props.item != null ? this.props.item.comments : null)}*/}
+        {/*<RenderComments comments={props.item != null ? props.item.comments : null}/>*/}
+        <RenderComments comments={ props.item?.comments}/>
+      </div>
+    </div>
+  );
+}
 
-  componentDidMount() {
-    console.log("ItemDetail componentDidMount es invocado");
-  }
-
-  componentDidUpdate() {
-    console.log("ItemDetail componentDidUpdate es invocado");
-  }
-
-  render() {
-    console.log("ItemDetail render es invocado");
+function RenderItem({item}) {// destructuring js
+  if (item != null) {
     return (
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          {this.renderItem(this.props.item)}
-        </div>
-        <div className="col-12 col-md-5 m-1">
-          {this.renderComments(this.props.item != null ? this.props.item.comments : null)}
-        </div>
+      <div key={item.id} className="col-8 m-auto">
+        <Card>
+          <Card.Header>
+            <Card.Title>{item.name}</Card.Title>
+          </Card.Header>
+          <Card.Img style={{width: '80%'}} className='m-4' src={item.image} alt={item.name}/>
+          <Card.Body>
+            <Card.Text>
+              {item.requisites.map((requisite, index) => (<span key={index}>{requisite}</span>))}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
       </div>
     );
+
+  } else {
+    return (
+      <div/>
+    );
   }
-
-  renderItem(item) {
-    if (item != null) {
-      return (
-        <div key={item.id} className="col-8 m-auto">
-          <Card>
-            <Card.Header>
-              <Card.Title>{item.name}</Card.Title>
-            </Card.Header>
-            <Card.Img style={{width: '80%'}} className='m-4' src={item.image} alt={item.name}/>
-            <Card.Body>
-              <Card.Text>
-                {item.requisites.map((requisite, index) => (<span key={index}>{requisite}</span>))}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-
-        </div>
-      );
-
-    } else {
-      return (
-        <div/>
-      );
-    }
-  }
-
-  renderComments(comments) {
-    if (comments != null) {
-      let commentsElements = comments.map((comment) => {
-        return (
-          <div key={comment.id}>
-            <li key={comment.id} className="list-unstyled">
-              <ul className="list-unstyled">
-                <li>{comment.comment}</li>
-                <li>-- {comment.author} , {new Intl.DateTimeFormat('en-US', {
-                  year: 'numeric',
-                  month: 'short',
-                  day: '2-digit'
-                }).format(new Date(Date.parse(comment.date)))}</li>
-              </ul>
-            </li>
-          </div>
-        )
-      });
-      return (
-        <div>
-          <h4>Comments</h4>
-          {commentsElements}
-        </div>
-      );
-    } else {
-      return (
-        <div/>
-      );
-    }
-  }
-
 }
+
+function RenderComments({comments}) {
+  if (comments != null) {
+    let commentsElements = comments.map((comment) => {
+      return (
+        <div key={comment.id}>
+          <li key={comment.id} className="list-unstyled">
+            <ul className="list-unstyled">
+              <li>{comment.comment}</li>
+              <li>-- {comment.author} , {new Intl.DateTimeFormat('en-US', {
+                year: 'numeric',
+                month: 'short',
+                day: '2-digit'
+              }).format(new Date(Date.parse(comment.date)))}</li>
+            </ul>
+          </li>
+        </div>
+      )
+    });
+    return (
+      <div>
+        <h4>Comments</h4>
+        {commentsElements}
+      </div>
+    );
+  } else {
+    return (
+      <div/>
+    );
+  }
+}
+
 
 export default ItemDetail;

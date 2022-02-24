25a5dd0fe5edb3136a5b53f5de444cf8fd9d416e
tercere <tercere@labcorp.com>
2022-02-24 18:55:21 -0400

    simple rename

--- src/components/CatalogComponent.js
+++ src/components/CatalogComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Card} from "react-bootstrap";
 
-function RenderMenuItem({item, onClick}) {
+function RenderCatalogItem({item, onClick}) {
   return (
     <Card text="primary" style={{width: "90%"}} className="bg-dark bg-opacity-25"
                 onClick={()=>onClick(item.id)}>
@@ -19,7 +19,7 @@ const Catalog = (props) => {
   let catalog = props.items.map(item => {
     return (
       <div key={item.id} className="col-12 col-md-5 m-1">
-        <RenderMenuItem onClick={props.onClick} item={item}/>
+        <RenderCatalogItem onClick={props.onClick} item={item}/>
       </div>
     );
   });

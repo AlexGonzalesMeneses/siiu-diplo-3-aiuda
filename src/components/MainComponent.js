3405ec5e7c47a81f1e07bc8c1df0fe7e23accc68
tercere <tercere@labcorp.com>
2022-02-24 18:34:39 -0400

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,13 +5,19 @@ import Footer from "./FooterComponent";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./HomeComponent";
 import Catalog from "./CatalogComponent";
+import Contact from "./ContactComponent";
+import {COMMENTS} from "../shared/comments";
+import {EMPLOYEES} from "../shared/employees";
 
 class Main extends Component {
 
   constructor(props, context) {
     super(props, context);
     this.state = {
-      items: ITEMS
+      items: ITEMS,
+      comments: COMMENTS,
+      employees: EMPLOYEES
+
     }
     console.log("Main constructor es invocado");
   }
@@ -32,8 +38,13 @@ class Main extends Component {
       <div>
         <Header/>
         <Routes>
-          <Route path='/home' element={<HomePage/>}/>
+          <Route path='/home' element={<Home
+            item={this.state.items.filter(item => item.featured)[0]}
+            employee={this.state.employees.filter(employee => employee.featured)[0]}
+          />
+          }/>
           <Route exact path='/catalog' element={<Catalog items={this.state.items}/>}/>
+          <Route exact path='/contactus' element={<Contact/>}/>
           <Route
             path="*"
             element={<HomePage/>}/>
--- src/shared/items.js
+++ src/shared/items.js
@@ -6,43 +6,12 @@ export const ITEMS = [
     category: 'certificado',
     label: 'diplomado',
     price: '200',
+    featured: true,
     description: 'El certificado de Diplomado es un documento que se otorga a quienes han logrado su titulacion en grado de diplomado',
     requisites: [
       "𝗖𝗲́𝗱𝘂𝗹𝗮 𝗱𝗲 𝗜𝗱𝗲𝗻𝘁𝗶𝗱𝗮𝗱 𝘃𝗶𝗴𝗲𝗻𝘁𝗲 𝗼 𝗗𝗡𝗜 𝘆/𝗼 𝗽𝗮𝘀𝗮𝗽𝗼𝗿𝘁𝗲 𝘃𝗶𝗴𝗲𝗻𝘁𝗲",
       "𝗨𝗻𝗮 (𝟭) 𝗳𝗼𝘁𝗼𝗴𝗿𝗮𝗳𝗶́𝗮 𝘁𝗮𝗺𝗮𝗻̃𝗼 𝟰 𝘅 𝟱; 𝗳𝗼𝗻𝗱𝗼 𝗔𝘇𝘂𝗹 𝗠𝗮𝗿𝗶𝗻𝗼",
       "𝗙𝗼𝘁𝗼𝗰𝗼𝗽𝗶𝗮 𝗱𝗲𝗹 𝗗𝗶𝗽𝗹𝗼𝗺𝗮 𝗮𝗰𝗮𝗱𝗲́𝗺𝗶𝗰𝗼 𝗱𝗲 𝗹𝗶𝗰𝗲𝗻𝗰𝗶𝗮𝘁𝘂𝗿𝗮 𝗼 𝘁𝗶́𝘁𝘂𝗹𝗼 𝗱𝗲 𝗲𝘅𝘁𝗿𝗮𝗻𝗷𝗲𝗿𝗼"
-    ],
-    comments: [
-      {
-        rating: 5,
-        comment: "No existe señaletica para dirigirse a la ventanilla de revision de documentos fisicos",
-        author: "Ana Castro",
-        date: "2021-10-16T17:57:28.556094Z"
-      },
-      {
-        rating: 4,
-        comment: "El horario de recojo de documentos deberia extederse tambien por la tarde",
-        author: "Tania Barrancos",
-        date: "2021-09-05T17:57:28.556094Z"
-      },
-      {
-        rating: 3,
-        comment: "El costo deberia estar incluido en las cuotas del programa",
-        author: "Michael Rodriguez",
-        date: "2020-02-13T17:57:28.556094Z"
-      },
-      {
-        rating: 4,
-        comment: "Hay mucha demora en la firma de autoridades",
-        author: "Diana Cardozo",
-        date: "2020-12-02T17:57:28.556094Z"
-      },
-      {
-        rating: 2,
-        comment: "Deberian permitir el tramite con apoderados legales",
-        author: "Mario Gutierrez",
-        date: "2019-12-02T17:57:28.556094Z"
-      }
     ]
   },
   {
@@ -52,44 +21,13 @@ export const ITEMS = [
     category: 'certificado',
     label: 'maestria',
     price: '300',
+    featured: false,
     description: 'El certificado de Maestria es un documento que se otorga a quienes han logrado su titulacion en grado de magister',
     requisites: [
       "𝗖𝗲́𝗱𝘂𝗹𝗮 𝗱𝗲 𝗜𝗱𝗲𝗻𝘁𝗶𝗱𝗮𝗱 𝘃𝗶𝗴𝗲𝗻𝘁𝗲 𝗼 𝗗𝗡𝗜 𝘆/𝗼 𝗽𝗮𝘀𝗮𝗽𝗼𝗿𝘁𝗲 𝘃𝗶𝗴𝗲𝗻𝘁𝗲",
       "𝗨𝗻𝗮 (𝟭) 𝗳𝗼𝘁𝗼𝗴𝗿𝗮𝗳𝗶́𝗮 𝘁𝗮𝗺𝗮𝗻̃𝗼 𝟰 𝘅 𝟱; 𝗳𝗼𝗻𝗱𝗼 𝗔𝘇𝘂𝗹 𝗠𝗮𝗿𝗶𝗻𝗼",
       "𝗙𝗼𝘁𝗼𝗰𝗼𝗽𝗶𝗮 𝗱𝗲𝗹 𝗗𝗶𝗽𝗹𝗼𝗺𝗮 𝗮𝗰𝗮𝗱𝗲́𝗺𝗶𝗰𝗼 𝗱𝗲 𝗹𝗶𝗰𝗲𝗻𝗰𝗶𝗮𝘁𝘂𝗿𝗮 𝗼 𝘁𝗶́𝘁𝘂𝗹𝗼 𝗱𝗲 𝗲𝘅𝘁𝗿𝗮𝗻𝗷𝗲𝗿𝗼",
       "𝗔𝗰𝘁𝗮 𝗱𝗲 𝗱𝗲𝗳𝗲𝗻𝘀𝗮 𝗱𝗲𝗹 𝘁𝗿𝗮𝗯𝗮𝗷𝗼 𝗱𝗲 𝗴𝗿𝗮𝗱𝗼"
-    ],
-    comments: [
-      {
-        rating: 4,
-        comment: "El acta de defensa no deberia ser requerido sino mas bien actualizado en el sistema",
-        author: "Pedro Luna",
-        date: "2022-02-16T17:57:28.556094Z"
-      },
-      {
-        rating: 4,
-        comment: "El horario de recojo de documentos deberia extederse tambien por la tarde",
-        author: "Tania Barrancos",
-        date: "2021-09-05T17:57:28.556094Z"
-      },
-      {
-        rating: 3,
-        comment: "El costo deberia estar incluido en las cuotas del programa",
-        author: "Michael Rodriguez",
-        date: "2020-02-13T17:57:28.556094Z"
-      },
-      {
-        rating: 4,
-        comment: "Hay mucha demora en la firma de autoridades",
-        author: "Diana Cardozo",
-        date: "2020-12-02T17:57:28.556094Z"
-      },
-      {
-        rating: 2,
-        comment: "Deberian permitir el tramite con apoderados legales",
-        author: "Mario Gutierrez",
-        date: "2019-12-02T17:57:28.556094Z"
-      }
     ]
   },
   {
@@ -99,43 +37,12 @@ export const ITEMS = [
     category: 'certificado',
     label: 'supletorio',
     price: '400',
+    featured: false,
     description: 'El certificado Supletorio es un documento que se otorga a quienes han justificado de forma pertinente la necesidad del supletorio',
     requisites: [
       "𝗖𝗲́𝗱𝘂𝗹𝗮 𝗱𝗲 𝗜𝗱𝗲𝗻𝘁𝗶𝗱𝗮𝗱 𝘃𝗶𝗴𝗲𝗻𝘁𝗲 𝗼 𝗗𝗡𝗜 𝘆/𝗼 𝗽𝗮𝘀𝗮𝗽𝗼𝗿𝘁𝗲 𝘃𝗶𝗴𝗲𝗻𝘁𝗲",
       "𝗨𝗻𝗮 (𝟭) 𝗳𝗼𝘁𝗼𝗴𝗿𝗮𝗳𝗶́𝗮 𝘁𝗮𝗺𝗮𝗻̃𝗼 𝟰 𝘅 𝟱; 𝗳𝗼𝗻𝗱𝗼 𝗔𝘇𝘂𝗹 𝗠𝗮𝗿𝗶𝗻𝗼",
       "𝗙𝗼𝘁𝗼𝗰𝗼𝗽𝗶𝗮 𝗱𝗲𝗹 𝗗𝗶𝗽𝗹𝗼𝗺𝗮 𝗮𝗰𝗮𝗱𝗲́𝗺𝗶𝗰𝗼 𝗱𝗲 𝗹𝗶𝗰𝗲𝗻𝗰𝗶𝗮𝘁𝘂𝗿𝗮 𝗼 𝘁𝗶́𝘁𝘂𝗹𝗼 𝗱𝗲 𝗲𝘅𝘁𝗿𝗮𝗻𝗷𝗲𝗿𝗼"
-    ],
-    comments: [
-      {
-        rating: 5,
-        comment: "La demanda deberia tener validez de 1 semanas y no 24 horas",
-        author: "Daniel Marquez",
-        date: "2021-11-17T17:57:28.556094Z"
-      },
-      {
-        rating: 4,
-        comment: "El horario de recojo de documentos deberia extederse tambien por la tarde",
-        author: "Tania Barrancos",
-        date: "2021-09-05T17:57:28.556094Z"
-      },
-      {
-        rating: 3,
-        comment: "El costo deberia estar incluido en las cuotas del programa",
-        author: "Michael Rodriguez",
-        date: "2020-02-13T17:57:28.556094Z"
-      },
-      {
-        rating: 4,
-        comment: "Hay mucha demora en la firma de autoridades",
-        author: "Diana Cardozo",
-        date: "2020-12-02T17:57:28.556094Z"
-      },
-      {
-        rating: 2,
-        comment: "Deberian permitir el tramite con apoderados legales",
-        author: "Mario Gutierrez",
-        date: "2019-12-02T17:57:28.556094Z"
-      }
     ]
   },
   {
@@ -145,42 +52,11 @@ export const ITEMS = [
     category: 'certificado',
     label: 'diplomado',
     price: '200',
+    featured: false,
     description: 'El certificado de Diplomado es un documento que se otorga a quienes han logrado su titulacion en grado de diplomado',
     requisites: [
       "𝗖𝗲́𝗱𝘂𝗹𝗮 𝗱𝗲 𝗜𝗱𝗲𝗻𝘁𝗶𝗱𝗮𝗱 𝘃𝗶𝗴𝗲𝗻𝘁𝗲 𝗼 𝗗𝗡𝗜 𝘆/𝗼 𝗽𝗮𝘀𝗮𝗽𝗼𝗿𝘁𝗲 𝘃𝗶𝗴𝗲𝗻𝘁𝗲",
       "𝗟𝗮 𝘀𝗼𝗹𝗶𝗰𝗶𝘁𝘂𝗱 𝘀𝗲𝗿𝗮 𝗮𝘁𝗲𝗻𝗱𝗶𝗱𝗮 𝘀𝗼𝗹𝗮𝗺𝗲𝗻𝘁𝗲 𝘀𝗶 𝗲𝗹 𝘁𝗿𝗮𝗯𝗮𝗷𝗼 𝗳𝗶𝗻𝗮𝗹 𝘀𝗲 𝗲𝗻𝗰𝘂𝗲𝗻𝘁𝗿𝗮 𝗿𝗲𝗴𝗶𝘀𝘁𝗿𝗮𝗱𝗼 𝗲𝗻 𝗹𝗮 𝗽𝗮𝗴𝗶𝗻𝗮 𝗗𝗗𝗶𝗴𝗶𝘁𝗮𝗹 -𝗨𝗺𝘀𝘀",
-    ],
-    comments: [
-      {
-        rating: 2,
-        comment: "El tiempo de registro en DDigital deberia ser mas corto",
-        author: "Milton Chura",
-        date: "2021-10-16T17:57:28.556094Z"
-      },
-      {
-        rating: 4,
-        comment: "El horario de recojo de documentos deberia extederse tambien por la tarde",
-        author: "Tania Barrancos",
-        date: "2021-09-05T17:57:28.556094Z"
-      },
-      {
-        rating: 3,
-        comment: "El costo deberia estar incluido en las cuotas del programa",
-        author: "Michael Rodriguez",
-        date: "2020-02-13T17:57:28.556094Z"
-      },
-      {
-        rating: 4,
-        comment: "Hay mucha demora en la firma de autoridades",
-        author: "Diana Cardozo",
-        date: "2020-12-02T17:57:28.556094Z"
-      },
-      {
-        rating: 2,
-        comment: "Deberian permitir el tramite con apoderados legales",
-        author: "Mario Gutierrez",
-        date: "2019-12-02T17:57:28.556094Z"
-      }
     ]
   }
 ];
